96a723965045f91ed07b30a626d203c850e1855f
saruaralam-swd <saruaralam2001@gmail.com>
2023-08-16 21:23:21 +0600

    json web token done

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-require("colors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+require("colors");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,30 +12,41 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Database Connected
+// Database Connection
 const uri = process.env.DB_URI;
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
 async function run() {
   try {
-    const userCollection = client.db("AirCnc").collection("users");
+    const userCollection = client.db("AirCnc").collections("users");
+
+    // create token
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const email = user?.email;
+      const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+        expiresIn: "30d",
+      });
+      res.send({ accessToken: token });
+    });
+
+    // database connection check
+    client.connect();
     console.log("Database Connected".yellow);
-  } finally {
-    await client.close();
+  } catch (error) {
+    console.log(error.message.bgYellow);
   }
 }
-run().catch((err) => console.log(err.name, err.message));
+run().catch((error) => console.log(error.name.bgRed, error.message));
 
 app.get("/", (req, res) => {
-  res.send("AirCnc server is running");
+  res.send("Aircnc Server is running...");
 });
 
 app.listen(port, () => {
-  console.log(`server is running on the port ${port}`.cyan);
+  console.log(`Server is running...on ${port}`.cyan);
 });
